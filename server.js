660deefbd0d4eea42be1463e9e99c0872763a0bc
Myrfion <32075241+Myrfion@users.noreply.github.com>
2021-09-26 22:15:14 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+/*********************************************************************************
+ * WEB322 – Assignment 1
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Name: Tymur Levtsun Student ID: 109570200 Date: 26 September 2021
+ *
+ * Online (Heroku) URL: _______________________________________________________
+ *
+ ********************************************************************************/
+
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => {
+  res.send("Tymur Levtsun - 109570200")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
